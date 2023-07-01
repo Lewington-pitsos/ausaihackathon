333bbdcb5eb41896fd72f717c59bab7923ecf8cf
Lewington-pitsos <lewingtonpitsos@gmail.com>
2023-07-01 11:18:01 +1000

    make reboot respect url

--- commands/reboot/index.mjs
+++ commands/reboot/index.mjs
@@ -2,5 +2,8 @@ import { boot } from "../../util/screens.js";
 
 const output = "Okay ;_;";
 
+const pageHref = window.location.search;
+const searchParams = new URLSearchParams(pageHref.substring(pageHref.indexOf('?')));
+
 export { output };
-export default () => boot();
+export default () => boot(searchParams.get('goto'));
